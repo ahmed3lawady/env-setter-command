2663f8f89f8331e5c55d9eaf40431d5ac96d7607
Ahmed El-Awady <ahmed.osama@areebgroup.com>
2019-02-25 19:35:57 +0200

    add the attribute if not found

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ All notable changes to this project will be documented in this file.
 
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/).
 
+## [1.0.1] - 2019-02-25
+### Added
+- Add the attribute if not found.
+
 ## [1.0.0] - 2019-02-25
 
 - Initiation.
--- src/EnvSetter.php
+++ src/EnvSetter.php
@@ -3,6 +3,7 @@
 namespace Ahmed3lawady\EnvSetter;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Storage;
 
 class EnvSetter extends Command
 {
@@ -37,15 +38,20 @@ class EnvSetter extends Command
      */
     public function handle()
     {
-        $envPath = app()->environmentPath().'/.env';
-        if (!\File::exists($envPath)){
-            \File::copy(app()->environmentPath().'/.env.example', $envPath);
+        if (!file_exists(app()->environmentFilePath())){
+            Storage::copy(app()->environmentPath().'/.env.example', app()->environmentFilePath());
         }
 
-        $envContent = file_get_contents($envPath);
-        preg_match('/^'.$this->argument('key').'[^\r\n]*/m', $envContent, $line);
-        $newLine = $this->argument('key').'='.$this->argument('value');
-        $set = file_put_contents($envPath, str_replace($line[0], $newLine, $envContent));
+        $envContent = file_get_contents(app()->environmentFilePath());
+        preg_match('/^' . $this->argument('key') . '[^\r\n]*/m', $envContent, $line);
+        $newLine = $this->argument('key') . '=' . $this->argument('value');
+
+        if (count($line)){
+            $set = file_put_contents(app()->environmentFilePath(), str_replace($line[0], $newLine, $envContent));
+        } else {
+            $set = file_put_contents(app()->environmentFilePath(), "\n\n" . $newLine, FILE_APPEND);
+        }
+
         echo ($set) ?
             $this->argument('key')." value has been updated.\n" :
             "Error, ".$this->argument('key')." value doesn't changed!\n";
